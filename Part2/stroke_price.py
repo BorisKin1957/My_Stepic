203015bf43c507385eb9bb0c7f26e5a622156165
Boris Kin <nicko@sibnet.ru>
2023-01-19 20:39:09 +0700

    Стоимость строки

--- Part2/stroke_price.py
+++ Part2/stroke_price.py
@@ -1,16 +1,16 @@
-'''для подсчета стоимости строки, исходя из того,
-что один любой символ(в том числе пробел стоит 6060 копеек.'''
+'''подсчет стоимости строки, исходя из того,
+что один любой символ(в том числе пробел) стоит 60 копеек.'''
 
 def price(stroke, char_price):
 
     a = len(stroke) * char_price
-    b = int(a // 100)
-    c = int(a % 100)
+    b = a // 100
+    c = a % 100
 
-    price = str(b) + ' р. ' + str(c) + ' коп.'
+    price = [b, 'р.', c, 'коп.']
 
     return price
 
 char_price = 60
 
-print(price(input(), char_price))
+print(*price(input(), char_price))
