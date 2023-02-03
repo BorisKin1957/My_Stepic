f7f45b796025b41a883c7b092018d4060e88a094
Boris Kin <nicko@sibnet.ru>
2023-02-03 11:24:16 +0700

    Заполнение 4

---
+++ Part2/zapolnenie_4.py
@@ -0,0 +1,30 @@
+'''Заполнение 4
+
+На вход программе подается натуральное число n.
+программa создает матрицу размером n×n
+заполнив её в соответствии с образцом:
+1 1 1 1 1 1 1 1 1
+0 1 1 1 1 1 1 1 0
+0 0 1 1 1 1 1 0 0
+0 0 0 1 1 1 0 0 0
+0 0 0 0 1 0 0 0 0
+0 0 0 1 1 1 0 0 0
+0 0 1 1 1 1 1 0 0
+0 1 1 1 1 1 1 1 0
+1 1 1 1 1 1 1 1 1 '''
+
+n = int(input())
+
+matrix = [[_ for _ in range(n)] for _ in range(n)]
+
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        matrix[i][j] = 0
+        if (i < j and i < n - 1 - j) or (i > j and i > n - 1 - j):
+            matrix[i][j] = 1
+        elif i == j or i == n - 1 - j:
+            matrix[i][j] = 1
+for row in matrix:
+    for col in row:
+        print(col, end=' ')
+    print()
