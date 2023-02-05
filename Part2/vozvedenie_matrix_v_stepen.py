3b0c85c80cba588d1d6b802fc47dcc3e51b81c9d
Boris Kin <nicko@sibnet.ru>
2023-02-05 23:03:37 +0700

    Возведение матрицы в степень

---
+++ Part2/vozvedenie_matrix_v_stepen.py
@@ -0,0 +1,36 @@
+'''Возведение матрицы в степень 🌶️
+
+программa возводит квадратную матрицу в m-ую степень.
+
+Формат входных данных
+На вход программе подаётся натуральное число n — количество строк и столбцов в матрице,
+затем элементы матрицы, затем натуральное число m.'''
+
+def mult_matrix(matrix1, matrix2, n):
+    matrix = []
+    for i in range(n):
+        row = []
+        for r in range(n):
+            summ = 0
+            for j in range(n):
+                mult = matrix1[i][j] * matrix2[j][r]
+                summ += mult
+            row.append(summ)
+        matrix.append(row)
+
+    return matrix
+
+
+n = int(input())
+matrix_in = [[int(i) for i in input().split()] for _ in range(n)]
+
+k = int(input())
+matrix = mult_matrix(matrix_in, matrix_in, n)
+
+for i in range(k - 2):
+    matrix = mult_matrix(matrix, matrix_in, n)
+
+for i in range(n):
+    for j in range(n):
+        print(matrix[i][j], end=' ')
+    print()
