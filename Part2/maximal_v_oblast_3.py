ba2887c2852d3b764021872b71212b4966a04e99
Boris Kin <nicko@sibnet.ru>
2023-02-06 11:27:50 +0700

    Максимальный в области 3

---
+++ Part2/maximal_v_oblast_3.py
@@ -0,0 +1,18 @@
+'''Максимальный в области 3
+выводит максимальный элемент в
+области правее гл диагонали квадратной матрицы'''
+
+n = int(input())
+matrix = []
+
+for _ in range(n):
+    row = [int(i) for i in input().split()]
+    matrix.append(row)
+
+s = []
+for i in  range(n):
+    for j in range(n):
+        if i <= j:
+            s.append(matrix[i][j])
+
+print(max(s))
