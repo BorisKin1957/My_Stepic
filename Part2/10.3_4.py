7c9568e8590e90260caf8ffbe4b47344cab60a0b
Boris Kin <nicko@sibnet.ru>
2023-02-24 17:11:36 +0700

    Дополните приведенный код

---
+++ Part2/10.3_4.py
@@ -0,0 +1,16 @@
+'''Дополните приведенный код, чтобы он вывел наиболее часто встречающееся слово строки s.
+Если таких слов несколько, должно быть выведено то, что меньше в лексикографическом порядке.'''
+
+s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley'
+
+l = s.split()
+a = 1
+for key in l:
+    if l.count(key) > a:
+        result = {}
+        result[key] = l.count(key)
+        a = l.count(key)
+    elif l.count(key) == a and key not in result:
+        result[key] = a
+
+print(min(result))
