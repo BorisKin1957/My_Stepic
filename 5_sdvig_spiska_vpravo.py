3c2ac1098381f46dad7880f883f07bd3016b16d2
Boris Kin <nicko@sibnet.ru>
2023-03-04 18:18:35 +0700

    Игрался

--- 5_sdvig_spiska_vpravo.py
+++ 5_sdvig_spiska_vpravo.py
@@ -1,11 +1,11 @@
-s = [i  for i in range(1, 11)]
-step = 1
-
-for i in range(step):
-    tmp = []
-    tmp.append(s[-1])
-    for j in range(len(s) - 1):
-        tmp.append(s[j])
-    s = tmp
+s = [i  for i in range(1, 6)]
+step = 7
+while len(s) > 1:
+    for i in range(step - 1):
+        tmp = []
+        tmp.append(s[-1])
+        for j in range(len(s) - 1):
+            tmp.append(s[j])
+        s = tmp[1:]
 
 print(s)
