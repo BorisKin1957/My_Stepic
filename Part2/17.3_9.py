22dc3fecf477d684319c693d06dd8bd956d2d1e0
Boris Kin <nicko@sibnet.ru>
2023-04-10 17:20:51 +0700

    Работа с текстовыми файлами

---
+++ Part2/17.3_9.py
@@ -0,0 +1,63 @@
+'''
+CSV-файл
+
+Вам доступен CSV-файл data.csv, содержащий информацию в csv формате.
+Напишите функцию read_csv для чтения данных из этого файла.
+Она должна возвращать список словарей, интерпретируя первую строку
+как имена ключей, а каждую последующую строку как значения этих ключей.
+
+Формат входных данных
+На вход программе ничего не подается.
+
+Формат выходных данных
+Программа должна содержать реализованную функцию read_csv.
+
+Примечание 1. Вызывать функцию read_csv не нужно.
+
+Примечание 2. Функция read_csv не должна принимать аргументов.
+
+Примечание 2. Подробнее прочитать про CSV-файлы можно тут.
+
+Примечание 3. Считайте, что все ключи и значения по этим ключам в
+результирующем словаре имеют строковый тип (str).
+
+Примечание 4. Указанный файл можно скачать по ссылке.
+
+Примечание 5. Если бы файл data.csv содержал информацию
+
+name,address,age
+George,4312 Abbey Road,22
+John,54 Love Ave,21
+
+то вызов функции read_csv() вернул бы список:
+
+[{'name': 'George', 'address': '4312 Abbey Road', 'age': '22'},
+{'name': 'John', 'address': '54 Love Ave', 'age': '21'}]
+
+'''
+
+def read_csv():
+    with open('data.csv') as file:
+        key_l = file.readline().strip().split(',')
+        csv_list = []
+        for line in file:
+            value_l = line.strip().split(',')
+            sl = {}
+            for i in range(len(key_l)):
+                s = {key_l[i]: value_l[i]}
+                sl.update(s)
+            csv_list.append(sl)
+
+    return csv_list
+
+print(read_csv())
+
+'''ПРАВИЛЬНО следовало создавать словарь через ZIP 
+о такой функции я и не вспомнил. 
+РЕШЕНИЕ:
+
+def read_csv():
+    with open('data.csv') as file:
+        keys = file.readline().strip().split(',')
+        return [dict(zip(keys, line.strip().split(','))) for line in file]
+'''
